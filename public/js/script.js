191f2c46713ef913b15f7da6d9f87a046e45b0a0
arclps <aponcesanga@gmail.com>
2022-12-05 17:38:34 -0500

    a

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,25 @@
-(function(document) {
+(function (document) {
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
 
-  document.addEventListener('click', function(e) {
+  document.addEventListener('click', function (e) {
     var target = e.target;
 
-    if(!checkbox.checked ||
-       sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
+    if (!checkbox.checked ||
+      sidebar.contains(target) ||
+      (target === checkbox || target === toggle)) return;
 
     checkbox.checked = false;
   }, false);
 })(document);
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let allButtons = document.querySelectorAll("button,[type=button]");
+
+  allButtons.forEach(button => {
+    button.className = 'btn btn-dark'
+  })
+})
