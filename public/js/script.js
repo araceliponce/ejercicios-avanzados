c33564f2dc74f61c9d2f3aad56c0aa7f8c389ae4
arclps <aponcesanga@gmail.com>
2022-12-06 07:59:24 -0500

    d

--- public/js/script.js
+++ public/js/script.js
@@ -17,13 +17,32 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  let allInputs = document.querySelectorAll("setion input");
+  let allInputs = document.querySelectorAll("section input");
   let allButtons = document.querySelectorAll("section button,[type=button]");
+  let allSections = document.querySelectorAll('article section[id]');
 
   allInputs.forEach(input => {
     input.className = 'form-control'
   })
   allButtons.forEach(button => {
     button.className = 'btn btn-dark'
+  })
+
+  console.log(allSections)
+
+  allSections.forEach(section => {
+    let anchor = document.createElement('a');
+    let sectionId = section.getAttribute('id');
+
+    console.log(sectionId)
+
+    anchor.setAttribute('href', `#${sectionId}`);
+    anchor.textContent = sectionId;
+    anchor.className = 'btn btn-info';
+
+    let h2 = document.querySelector('h2');
+
+    h2.parentElement.insertBefore(anchor, h2)
+    //https://developer.mozilla.org/en-US/docs/Web/API/Element/after
   })
 })
