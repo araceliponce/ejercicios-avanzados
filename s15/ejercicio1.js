7c90869e392395660402612b86f0fa5ed249f3b2
arclps <aponcesanga@gmail.com>
2022-12-21 10:08:16 -0500

    node & express js s15

---
+++ s15/ejercicio1.js
@@ -0,0 +1,15 @@
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+const http = require('http');
+
+const hostame = '127.0.0.1' //puedes colocar cualquier numero de hostame
+const port = 3000
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/plain') //aplication/json
+  response.end('araceli ponce sanga')
+})
+
+server.listen(port, hostame, () => {
+  console.log(`:::el servidor se esta ejecutando en http://${hostame}:${port}/`)
+})
